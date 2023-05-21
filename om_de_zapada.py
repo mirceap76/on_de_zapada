cc47c4e33a0ca4fa4ea81be68a7ba0547edc5883
mirceap76 <95441321+mirceap76@users.noreply.github.com>
2023-05-21 13:51:56 +0300

    Add files via upload

---
+++ om_de_zapada.py
@@ -0,0 +1,17 @@
+import time
+
+def danseaza():
+    for i in range(3):
+        print("   /\"\\")
+        print("  (   )")
+        print("  (   )")
+        print("  (   )")
+        print("   |||")
+        time.sleep(1)
+        print(" \\\|||//")
+        print("\\\\|  |  |  |//")
+        print(" \\\|  |  |//")
+        print("   |  |  |")
+        time.sleep(1)
+
+danseaza()
